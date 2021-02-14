be6cafbdf537e1a788c7880055970356dbeb0570
laim0nas100 <aciukadsiunciate@gmail.com>
2021-02-14 23:56:53 +0200

    update

--- src/test/java/lt/lb/caller/test/TreeBuilder.java
+++ src/test/java/lt/lb/caller/test/TreeBuilder.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lt.lb.caller.test;
 
 import java.util.ArrayList;
@@ -15,7 +10,7 @@ import lt.lb.caller.CallerForBuilder;
 
 /**
  *
- * @author Lemmin
+ * @author laim0nas100
  */
 public class TreeBuilder {
 
